e5384b66d0278818516ecc73e898489245e2270f
dfuh18 <101522939+dfuh18@users.noreply.github.com>
2023-02-20 20:20:16 +0100

    fixed "darkmode bug"

--- darkmode.js
+++ darkmode.js
@@ -44,10 +44,22 @@ document.addEventListener("DOMContentLoaded", function(){
     document.querySelector('.fa-moon').style.display = 'inline-block';
     document.querySelector('.fa-sun').style.display = 'none';
     document.querySelectorAll('.text-muted').forEach(el => el.classList.replace('text-muted', 'text-white'));
+    document.getElementById("currentpagelink").classList.replace('link-secondary', 'link-light');
+    document.getElementById("difpagelink").classList.replace('link-dark', 'link-secondary');
+    document.getElementById("difpagelink1").classList.replace('link-dark', 'link-secondary');
+    document.getElementById("difpagelink2").classList.replace('link-dark', 'link-secondary');
+    document.getElementById("difpagelink3").classList.replace('link-dark', 'link-secondary');
+    language.style.color = "white";
   }else{
     document.body.classList.remove("dark-mode");
     document.querySelector('.fa-moon').style.display = 'none';
     document.querySelector('.fa-sun').style.display = 'inline-block';
     document.querySelectorAll('.text-white').forEach(el => el.classList.replace('text-white', 'text-muted'));
+    document.getElementById("currentpagelink").classList.replace('link-light', 'link-secondary');
+    document.getElementById("difpagelink").classList.replace('link-dark', 'link-secondary');
+    document.getElementById("difpagelink1").classList.replace('link-secondary', 'link-dark');
+    document.getElementById("difpagelink2").classList.replace('link-secondary', 'link-dark');
+    document.getElementById("difpagelink3").classList.replace('link-secondary', 'link-dark');
+    language.style.color = "black";
   }
 });
