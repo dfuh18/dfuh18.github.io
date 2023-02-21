348c2170a7c7ee338c0b96311a60fb4ae2f4a106
dfuh18 <101522939+dfuh18@users.noreply.github.com>
2023-02-21 13:55:57 +0100

    navbar und darkmode für neue htmls

--- darkmode.js
+++ darkmode.js
@@ -9,11 +9,16 @@ document.getElementById('darkModeToggle').addEventListener('click', function() {
     // store the state in localStorage
     document.querySelectorAll('.text-muted').forEach(el => el.classList.replace('text-muted', 'text-white'));
     //change navbar color
-    document.getElementById("currentpagelink").classList.replace('link-secondary', 'link-light');
+    if(document.getElementById("currentpagelink")){
+      document.getElementById("currentpagelink").classList.replace('link-secondary', 'link-light')
+    };
     document.getElementById("difpagelink").classList.replace('link-dark', 'link-secondary');
     document.getElementById("difpagelink1").classList.replace('link-dark', 'link-secondary');
     document.getElementById("difpagelink2").classList.replace('link-dark', 'link-secondary');
     document.getElementById("difpagelink3").classList.replace('link-dark', 'link-secondary');
+    if (document.getElementById("NOcurrentpagelink")) {
+      document.getElementById("NOcurrentpagelink").classList.replace('link-dark', 'link-secondary');
+    }
     language.style.color = "white";
 
     if (document.getElementById("download") != null) document.getElementById("download").style.backgroundColor = "lightslategrey";
@@ -25,11 +30,16 @@ document.getElementById('darkModeToggle').addEventListener('click', function() {
     // store the state in localStorage
     document.querySelectorAll('.text-white').forEach(el => el.classList.replace('text-white', 'text-muted'));
 
-    document.getElementById("currentpagelink").classList.replace('link-light', 'link-secondary');
+    if(document.getElementById("currentpagelink")){
+      document.getElementById("currentpagelink").classList.replace('link-secondary', 'link-dark')
+    };
     document.getElementById("difpagelink").classList.replace('link-dark', 'link-secondary');
     document.getElementById("difpagelink1").classList.replace('link-secondary', 'link-dark');
     document.getElementById("difpagelink2").classList.replace('link-secondary', 'link-dark');
     document.getElementById("difpagelink3").classList.replace('link-secondary', 'link-dark');
+    if (document.getElementById("NOcurrentpagelink")) {
+      document.getElementById("NOcurrentpagelink").classList.replace('link-secondary', 'link-dark');
+    }
     language.style.color = "black";
 
     if (document.getElementById("download") != null)  document.getElementById("download").style.backgroundColor = "#edf5ff";
@@ -49,11 +59,16 @@ document.addEventListener("DOMContentLoaded", function(){
     document.querySelector('.fa-moon').style.display = 'inline-block';
     document.querySelector('.fa-sun').style.display = 'none';
     document.querySelectorAll('.text-muted').forEach(el => el.classList.replace('text-muted', 'text-white'));
-    document.getElementById("currentpagelink").classList.replace('link-secondary', 'link-light');
+    if(document.getElementById("currentpagelink")){
+      document.getElementById("currentpagelink").classList.replace('link-secondary', 'link-light')
+    };
     document.getElementById("difpagelink").classList.replace('link-dark', 'link-secondary');
     document.getElementById("difpagelink1").classList.replace('link-dark', 'link-secondary');
     document.getElementById("difpagelink2").classList.replace('link-dark', 'link-secondary');
     document.getElementById("difpagelink3").classList.replace('link-dark', 'link-secondary');
+    if (document.getElementById("NOcurrentpagelink")) {
+      document.getElementById("NOcurrentpagelink").classList.replace('link-dark', 'link-secondary');
+    }
 
     if (document.getElementById("download") != null) document.getElementById("download").style.backgroundColor = "#lightslategrey";
     language.style.color = "white";
@@ -64,12 +79,16 @@ document.addEventListener("DOMContentLoaded", function(){
     document.querySelector('.fa-moon').style.display = 'none';
     document.querySelector('.fa-sun').style.display = 'inline-block';
     document.querySelectorAll('.text-white').forEach(el => el.classList.replace('text-white', 'text-muted'));
-    document.getElementById("currentpagelink").classList.replace('link-light', 'link-secondary');
+    if(document.getElementById("currentpagelink")){
+      document.getElementById("currentpagelink").classList.replace('link-secondary', 'link-dark')
+    };
     document.getElementById("difpagelink").classList.replace('link-dark', 'link-secondary');
     document.getElementById("difpagelink1").classList.replace('link-secondary', 'link-dark');
     document.getElementById("difpagelink2").classList.replace('link-secondary', 'link-dark');
     document.getElementById("difpagelink3").classList.replace('link-secondary', 'link-dark');
-
+    if (document.getElementById("NOcurrentpagelink")) {
+      document.getElementById("NOcurrentpagelink").classList.replace('link-secondary', 'link-dark');
+    }
 
     if (document.getElementById("download") != null)  document.getElementById("download").style.backgroundColor = "#edf5ff";
     language.style.color = "black";
