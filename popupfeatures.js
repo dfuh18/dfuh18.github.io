f85a566cc5ed369ffa5388843eb77af1dbc28f91
dfuh18 <101522939+dfuh18@users.noreply.github.com>
2023-02-24 10:20:43 +0100

    popup features

---
+++ popupfeatures.js
@@ -0,0 +1,29 @@
+document.getElementById("popup-0").addEventListener("click", function(){ 
+
+    window.open("https://studym8ai.app/tutorial.html#Definition", "Popup Window", "width=500,height=500"); 
+});
+
+document.getElementById("popup-1").addEventListener("click", function(){ 
+
+    window.open("https://studym8ai.app/tutorial.html#Short Summary", "Popup Window", "width=500,height=500"); 
+});
+
+document.getElementById("popup-2").addEventListener("click", function(){ 
+
+    window.open("https://studym8ai.app/tutorial.html#Short Summary", "Popup Window", "width=500,height=500"); 
+});
+
+document.getElementById("popup-3").addEventListener("click", function(){ 
+
+    window.open("https://studym8ai.app/tutorial.html#Create Bulletpoints", "Popup Window", "width=500,height=500"); 
+});
+
+document.getElementById("popup-4").addEventListener("click", function(){ 
+
+    window.open("https://studym8ai.app/tutorial.html#Answer Question", "Popup Window", "width=500,height=500"); 
+});
+
+document.getElementById("popup-5").addEventListener("click", function(){ 
+
+    window.open("https://studym8ai.app/tutorial.html#Create Flashcards", "Popup Window", "width=500,height=500"); 
+});
