d46b9322254be68ba710d212001731223efd1464
dfuh18 <101522939+dfuh18@users.noreply.github.com>
2024-02-19 15:59:30 +0100

    brute force update domain name

--- impressum.js
+++ impressum.js
@@ -1,4 +1,4 @@
 document.getElementById("impressum").addEventListener("click", function(){ 
 
-    window.open("https://studym8ai.app/impressum.html", "Popup Window", "width=500,height=500"); 
+    window.open("https://dfuh18.github.io/impressum.html", "Popup Window", "width=500,height=500"); 
 });
